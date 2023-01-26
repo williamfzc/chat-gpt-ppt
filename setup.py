7fa6448c57eeb410202f991a337095beb138adc6
williamfzc <178894043@qq.com>
2023-01-26 16:43:45 +0800

    release: 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chat-gpt-ppt",
-    version="0.1.1",
+    version="0.2.0",
     description="use ChatGPT to generate PPT automatically",
     author="williamfzc",
     author_email="williamfzc@foxmail.com",
