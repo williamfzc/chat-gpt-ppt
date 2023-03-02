0b1c9aa162917311d5ab46af1806fdf9738a2f01
williamfzc <178894043@qq.com>
2023-03-02 13:51:01 +0000

    feat: cmd args

--- cmd/cgp/main.go
+++ cmd/cgp/main.go
@@ -1,18 +1,28 @@
 package main
 
-import cgp "github.com/williamfzc/chat-gpt-ppt"
-import _ "github.com/williamfzc/chat-gpt-ppt/assets"
+import (
+	"flag"
+	"os"
+	"strings"
+
+	cgp "github.com/williamfzc/chat-gpt-ppt"
+	_ "github.com/williamfzc/chat-gpt-ppt/assets"
+)
 
 func main() {
-	token := ""
-	questions := []string{
-		"a",
-		"b",
-		"c",
-	}
+	tokenFile := flag.String("token", "./token.txt", "token file path")
+	topicFile := flag.String("topic", "./topic.txt", "topic file path")
+	outputFile := flag.String("output", "./output.ppt", "out path")
+	flag.Parse()
+
+	tokenBytes, err := os.ReadFile(*tokenFile)
+	panicIfErr(err)
+	topicContents, err := os.ReadFile(*topicFile)
+	panicIfErr(err)
+	questions := strings.Split(string(topicContents), "\n")
 
 	topics := make([]*cgp.Topic, 0)
-	c := cgp.NewClient(token)
+	c := cgp.NewClient(string(tokenBytes))
 	for _, eachTopic := range questions {
 		resp, err := c.AskTopic(eachTopic)
 		panicIfErr(err)
@@ -27,7 +37,7 @@ func main() {
 	for _, eachTopic := range topics {
 		renderer.AddTopic(eachTopic)
 	}
-	err := renderer.RenderFile("./output.ppt")
+	err = renderer.RenderFile(*outputFile)
 	panicIfErr(err)
 }
 
